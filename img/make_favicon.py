7f0311546e56cf0b4c08f01ca0af0a7c5c60bd8e
jakelevi1996 <jakelevi@hotmail.co.uk>
2025-02-08 01:37:05 +0000

    Add `img/icon_large.png`

--- img/make_favicon.py
+++ img/make_favicon.py
@@ -1,28 +1,35 @@
 from jutility import plotting
 
-mp = plotting.MultiPlot(
-    plotting.Subplot(
-        plotting.Circle(
-            [0, 0],
-            1,
-            ec=None,
-            fc="#000050",
+def make_icon(
+    dpi: float,
+    name: str,
+):
+    mp = plotting.MultiPlot(
+        plotting.Subplot(
+            plotting.Circle(
+                [0, 0],
+                1,
+                ec=None,
+                fc="#000050",
+            ),
+            plotting.Text(
+                *[0, -0.15],
+                "J",
+                fs=200,
+                fontfamily="monospace",
+                c="#fefae0",
+            ),
+            xlim=[-1, 1],
+            ylim=[-1, 1],
+            axis_square=True,
+            axis_off=True,
         ),
-        plotting.Text(
-            *[0, -0.15],
-            "J",
-            fs=200,
-            fontfamily="monospace",
-            c="#fefae0",
-        ),
-        xlim=[-1, 1],
-        ylim=[-1, 1],
-        axis_square=True,
-        axis_off=True,
-    ),
-    colour="#00000000",
-    pad=0,
-    figsize=[4, 4],
-    dpi=32/4,
-)
-mp.save("favicon", "img")
+        colour="#00000000",
+        pad=0,
+        figsize=[4, 4],
+        dpi=dpi,
+    )
+    mp.save(name, "img")
+
+make_icon(32/4, "favicon")
+make_icon(160, "icon_large")
